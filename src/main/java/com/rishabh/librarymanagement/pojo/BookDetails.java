316051934376ef3311c67b49a7c44795841d031a
Rishabh Singh <rishabhsingh@Rishabhs-MacBook-Air.local>
2021-08-30 20:48:20 +0530

    qw

--- src/main/java/com/rishabh/librarymanagement/pojo/BookDetails.java
+++ src/main/java/com/rishabh/librarymanagement/pojo/BookDetails.java
@@ -1,6 +1,5 @@
 package com.rishabh.librarymanagement.pojo;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.rishabh.librarymanagement.dao.Book;
 import lombok.Data;
@@ -9,7 +8,6 @@ import java.io.Serializable;
 import java.util.Date;
 
 @Data
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class BookDetails implements Serializable {
 
     @JsonUnwrapped
