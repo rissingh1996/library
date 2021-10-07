8ec7662ea2ecb3ed7e7e7cc42bec588640a8fd1e
Rishabh Singh <rishabhsingh@Rishabhs-MacBook-Air.local>
2021-10-07 20:00:14 +0530

    r

--- src/main/java/com/rishabh/librarymanagement/annotations/Authorization.java
+++ src/main/java/com/rishabh/librarymanagement/annotations/Authorization.java
@@ -8,5 +8,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Authorization {
+
     public String[] value() default "USER";
 }
